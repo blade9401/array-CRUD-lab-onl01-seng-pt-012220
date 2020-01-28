804c8aa5d0835f8aeb68e4a483d6012aae464232
Eric murray <ericwmurray1994@gmail.com>
2020-01-28 05:13:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,3 +14,8 @@ def add_element_to_start_of_array(array,element)
   add_element_to_start_of_array = ["array","element"]
   add_element_to_start_of_array.unshift("wow")
 end
+
+def remove_element_from_end_of_array(array)
+  remove_element_from_end_of_array = ["Arrays!"]
+  remove_element_from_end_of_array.pop
+end
