ea0e12f912635e2aa2eb3178aede5d4472bb1f15
Eric murray <ericwmurray1994@gmail.com>
2020-01-28 05:17:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,3 +26,6 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array,index)
+  retrieve_element_from_index = ["well","I", "am"]
+  retrieve_element_from_index(2)
+end
