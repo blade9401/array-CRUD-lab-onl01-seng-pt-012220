4759e37150bd96db8b2ca67350971d0f262da346
Eric murray <ericwmurray1994@gmail.com>
2020-01-28 05:31:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,7 @@ def retrieve_last_element_from_array(array)
   retrieve_last_element_from_array.last
 end
 
-def retrieve_first_element_of_array(array)
-  retrieve_first_element_of_array = ["wow"]
-  retrieve_first_element_of_array.first
+def retrieve_first_element_from_array(array)
+  retrieve_first_element_from_array = ["wow"]
+  retrieve_first_element_from_array.first
 end
